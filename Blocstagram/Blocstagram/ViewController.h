731ef073a5420db7488150f7742f5d2017732470
Paul Savage <p.a.savage@btinternet.com>
2015-10-02 17:17:25 +0100

    Initial Blockstagram commit

---
+++ Blocstagram/Blocstagram/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Blocstagram
+//
+//  Created by Paul Savage on 02/10/2015.
+//  Copyright (c) 2015 Paul Savage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Blocstagram/Blocstagram/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Blocstagram
+//
+//  Created by Paul Savage on 02/10/2015.
+//  Copyright (c) 2015 Paul Savage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
