cb3d2f418b05d83cbd7d94d788c0fb52c5a4aebb
Paul Savage <p.a.savage@btinternet.com>
2015-10-08 14:37:52 +0100

    Checkpoint 29 completed, complex data cells

---
+++ Blocstagram/Blocstagram/MediaTableViewCell.h
@@ -0,0 +1,19 @@
+//
+//  MediaTableViewCell.h
+//  Blocstagram
+//
+//  Created by Paul Savage on 08/10/2015.
+//  Copyright (c) 2015 Paul Savage. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class Media;
+
+@interface MediaTableViewCell : UITableViewCell
+
+@property (nonatomic, strong) Media *mediaItem;
+
++ (CGFloat)heightForMediaItem:(Media *)mediaItem width:(CGFloat)width;
+
+@end
