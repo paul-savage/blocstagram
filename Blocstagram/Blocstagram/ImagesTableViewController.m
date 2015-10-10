9a7462fd947ff5deb7555195655ac60af8bbee5b
Paul Savage <p.a.savage@btinternet.com>
2015-10-10 17:18:01 +0100

    Checkpoint 34 completed, retrieving images

--- Blocstagram/Blocstagram/ImagesTableViewController.m
+++ Blocstagram/Blocstagram/ImagesTableViewController.m
@@ -64,6 +64,20 @@
     }
 }
 
+- (CGFloat)tableView:(UITableView *)tableView estimatedHeightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    
+    Media *item = [DataSource sharedInstance].mediaItems[indexPath.row];
+    
+    if (item.image) {
+        
+        return 350;
+        
+    } else {
+        
+        return 150;
+    }
+}
+
 - (void)dealloc {
     
     [[DataSource sharedInstance] removeObserver:self forKeyPath:@"mediaItems"];
